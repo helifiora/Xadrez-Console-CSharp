d0ffb9a26aef09c834a8012eec3d787d42e7bfa8
Helielton Fioramonte <helifiora@gmail.com>
2019-07-09 21:20:49 -0300

    Correção Mudando o Nome do Metodo para MomentoPossivel

--- Chess-Console-CSharp/xadrez/PartidaDeXadrez.cs
+++ Chess-Console-CSharp/xadrez/PartidaDeXadrez.cs
@@ -182,7 +182,7 @@ namespace xadrez
 
         public void ValidarPosicaoDestino(Posicao origem, Posicao destino)
         {
-            if (!Tabuleiro.Peca(origem).PodeMoverPara(destino))
+            if (!Tabuleiro.Peca(origem).MovimentoPossivel(destino))
                 throw new TabuleiroException("Posição de Destino Inválida!");
         }
 
